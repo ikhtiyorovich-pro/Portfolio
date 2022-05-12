36348dc94f01555158794351235e765a5dc4256a
ikhtiyorovich-pro <muhammadali.fayzullayev@yandex.ru>
2022-05-12 22:14:18 +0500

    next changes

--- main.js
+++ main.js
@@ -3,43 +3,4 @@ window.addEventListener('DOMContentLoaded', function () {
       const header = document.querySelector('.top-wrapper__links-wrapper');
       header.classList.toggle('sticky', window.scrollY > 0)
     })
-  
-    const menuBtn = document.querySelector('.menu-btn')
-    const navigation = document.querySelector('.navigation')
-    const navigationItems = document.querySelectorAll('.navigation a')
-  
-    menuBtn.addEventListener('click', () => {
-      menuBtn.classList.toggle('active')
-      navigation.classList.toggle('active')
-    })
-  
-    navigationItems.forEach(navItem => {
-      navItem.addEventListener('click', () => {
-        menuBtn.classList.remove('active')
-        navigation.classList.remove('active')
-      })
-    })
-  
-    const scrollBtn = document.querySelector('.scrollToTop-btn')
-    window.addEventListener('scroll', () => {
-      scrollBtn.classList.toggle('active', window.scrollY > 500)
-    })
-    scrollBtn.addEventListener('click', () => {
-      document.body.scrollTop = 0;
-      document.documentElement.scrollTop = 0;
-    })
-  
-    window.addEventListener('scroll', () => {
-      let reveals = document.querySelectorAll('.reveal')
-  
-      for(let i = 0; i< reveals.length; i++) {
-        let windowHeight = window.innerHeight;
-        let revealTop = reveals[i].getBoundingClientRect().top;
-        let revealPoint = 50;
-  
-        if(revealTop < windowHeight - revealPoint) {
-          reveals[i].classList.add('active')
-        }
-      }
-    })
   })
